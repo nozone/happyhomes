844292b734c80a9667554a925692746ef42a63c3
Savanna Rovira <savanna.rovira@gmail.com>
2016-02-27 18:22:28 -0500

    Added the ajax script

---
+++ main.js
@@ -0,0 +1,15 @@
+$('#submit').click(addListings);
+
+$(function addListings(){
+	var $listings = $('#listings');
+
+	$.ajax({
+		type: 'GET',
+		url:
+		success: function(listings) {
+			$.each(listings, function(i, listing) {
+				$listings.append('<li> Address: ' + listing.street + '<br>' + listing.city + ', ' + list.state + ', ' + listing.zip + '<br>' + 'Monthly Rent: ' + listing.cost + '<br>' + 'Bedrooms: ' + listing.bed + '<br>' + 'Baths: ' + listing.bath + '<br>' + 'Livability Index: ' + listing.live + '</li>');
+			});
+		}
+	});
+});
