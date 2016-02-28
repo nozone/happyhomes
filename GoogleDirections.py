b15f51f0614bf20d3cba2ee1760e05ee16e8d4a5
Martin Folkoff <simpsonworld12@gmail.com>
2016-02-27 20:03:09 -0500

    adding geocoding and duration/distance info

--- GoogleDirections.py
+++ GoogleDirections.py
@@ -1,23 +1,26 @@
 import googlemaps
 from googlemaps import client as _client
-import json
+from googlemaps import convert
 
 class GoogleDirections:
 
 	def parseDirectionDump(self, directions):
-		aggregateMap = {'BUS':0,'SUBWAY':0,'METRO_RAIL':0}
+		aggregateMap = {'distance': 'N/A', 'duration':'N/A"', 'BUS':0,'SUBWAY':0,'METRO_RAIL':0}
 
 		legs = directions[0]["legs"]
 		print "there are " + str(len(legs)) + " legs \n"
 
 		for leg in legs:
 			print "there are " + str(len(leg["steps"])) + "steps"
+			distinanceInMeters = leg["distance"]["value"]
+			distanceInMiles = distinanceInMeters * 0.000621371192
+			aggregateMap['distance'] = distanceInMiles
+			aggregateMap['duration'] = leg["duration"]["value"]
+
 			for step in leg["steps"]:
 				if step["travel_mode"] == "TRANSIT":
 					num_stops = step["transit_details"]["num_stops"]
 					vehicle_type = step["transit_details"]["line"]["vehicle"]['type']
-					print vehicle_type
-					print num_stops
 					aggregateMap[vehicle_type] = aggregateMap[vehicle_type] + num_stops
 				else:
 					print "found a non-transit step"
@@ -41,17 +44,24 @@ class GoogleDirections:
 
 		return totalCost
 
+	def geoCodeAddress(self, client, address):
+		jsonResult = client.geocode(address)
+		lat = str(jsonResult[0]["geometry"]["bounds"]["northeast"]["lat"])
+		lng = str(jsonResult[0]["geometry"]["bounds"]["northeast"]["lng"])
+		latlng = lat + "," + lng
+		return latlng
 
 if __name__ == '__main__': 
 	dctobmore=["Washington DC, Baltimore"]
 	sample1=["38.918842,-77.011821", "38.906585,-77.039662"]
 
 	client = googlemaps.Client(key="AIzaSyD6z37kjR2tSz48dTM_SOEdwduLzuwImuo")
+	
 	directions = client.directions(sample1[0],sample1[1], mode="transit")
-
 	googleDirections = GoogleDirections()
+	#print googleDirections.geoCodeAddress(client, "Salisbury, MD")
 
 	aggregateData = googleDirections.parseDirectionDump(directions)
 
 	print aggregateData
-	print googleDirections.generateCostModel(aggregateData)
+	#print googleDirections.generateCostModel(aggregateData)
